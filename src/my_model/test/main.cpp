b126cc7e5677e11504904c61b951df0fa789f5cc
Uthappa <gayanuthappams@gmail.com>
2024-11-24 17:30:52 -0500

    Task 1.11 - Wrote the main.cpp file for the test.cpp file

---
+++ src/my_model/test/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-11-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
